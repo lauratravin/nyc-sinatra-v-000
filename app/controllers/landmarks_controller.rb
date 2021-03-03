e5108426fc611104cdbb7fe5fcf10d20d525e877
Laura Messa Travin <laura.travin@gmail.com>
2021-03-03 04:13:20 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -5,4 +5,19 @@ class LandmarksController < ApplicationController
      @landmarks = Landmark.all
      erb :'landmarks/index'
   end
+
+  get '/landmarks/new'  do
+    erb :'landmarks/new'
+  end
+
+  get '/landmarks/:id' do
+    @landmarks = Landmark.find(params[:id])
+    erb :'landmarks/show'
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmarks = Landmark.find(params[:id])
+    erb :'landmarks/edit'
+  end
+
 end
